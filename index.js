23e36e0745d573e69ac6d23ffee23a5894ec3314
Marc Görtz <mail@marcgoertz.de>
2017-04-15 14:57:21 +0200

    Add initial formatter.

---
+++ index.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const path = require('path');
+const chalk = require('chalk');
+const logSymbols = require('log-symbols');
+const plur = require('plur');
+const stringWidth = require('string-width');
+const ansiEscapes = require('ansi-escapes');
+
+module.exports = results => {
+  const lines = [];
+  let errorCount = 0;
+  let warningsCount = 0;
+  let maxLineWidth = 0;
+  let maxColumnWidth = 0;
+  let maxMessageWidth = 0;
+  let showLineNumbers = false;
+
+  results
+    .sort((a, b) => a.warnings.length - b.warnings.length)
+    .forEach(result => {
+      const warnings = result.warnings;
+
+      if (warnings.length === 0) {
+        return;
+      }
+
+      if (lines.length !== 0) {
+        lines.push({type: 'separator'});
+      }
+
+      const filePath = result.source;
+
+      lines.push({
+        type: 'header',
+        filePath,
+        relativeFilePath: path.relative('.', filePath),
+        firstLineCol: warnings[0].line + ':' + warnings[0].column
+      });
+
+      warnings
+        .sort((a, b) => {
+          if (a.severity === b.severity) {
+            if (a.line === b.line) {
+              return a.column < b.column ? -1 : 1;
+            }
+            return a.line < b.line ? -1 : 1;
+          } else if (a.severity === 2 && b.severity !== 2) {
+            return 1;
+          }
+          return -1;
+        })
+        .forEach(x => {
+          let message = x.text;
+
+          // Remove rule ID from message
+          message = message.replace(/\s\(.+\)$/g, '');
+
+          // Stylize inline code blocks
+          message = message.replace(/\B`(.*?)`\B|\B'(.*?)'\B/g, (m, p1, p2) => chalk.bold(p1 || p2));
+
+          const line = String(x.line || 0);
+          const column = String(x.column || 0);
+          const lineWidth = stringWidth(line);
+          const columnWidth = stringWidth(column);
+          const messageWidth = stringWidth(message);
+          let severity = 'warning';
+          if (x.severity === 2 || x.severity === 'error') {
+            severity = 'error';
+            errorCount++;
+          } else {
+            warningsCount++;
+          }
+
+          maxLineWidth = Math.max(lineWidth, maxLineWidth);
+          maxColumnWidth = Math.max(columnWidth, maxColumnWidth);
+          maxMessageWidth = Math.max(messageWidth, maxMessageWidth);
+          showLineNumbers = showLineNumbers || x.line || x.column;
+
+          lines.push({
+            type: 'message',
+            severity,
+            line,
+            lineWidth,
+            column,
+            columnWidth,
+            message,
+            messageWidth,
+            ruleId: x.rule || ''
+          });
+        });
+    });
+
+  let output = '\n';
+
+  if (process.stdout.isTTY && !process.env.CI) {
+    // Make relative paths Cmd+click'able in iTerm
+    output += ansiEscapes.iTerm.setCwd();
+  }
+
+  output += lines.map(x => {
+    if (x.type === 'header') {
+      // Add the line number so it's Cmd+click'able in some terminals
+      // Use dim & gray for terminals like iTerm that doesn't support `hidden`
+      const position = showLineNumbers ? chalk.hidden.dim.gray(`:${x.firstLineCol}`) : '';
+
+      return '  ' + chalk.underline(x.relativeFilePath + position);
+    }
+
+    if (x.type === 'message') {
+      const line = [
+        '',
+        x.severity === 'warning' ? logSymbols.warning : logSymbols.error,
+        ' '.repeat(maxLineWidth - x.lineWidth) + chalk.dim(x.line + chalk.gray(':') + x.column),
+        ' '.repeat(maxColumnWidth - x.columnWidth) + x.message,
+        ' '.repeat(maxMessageWidth - x.messageWidth) + chalk.gray.dim(x.ruleId)
+      ];
+
+      if (!showLineNumbers) {
+        line.splice(2, 1);
+      }
+
+      return line.join('  ');
+    }
+
+    return '';
+  }).join('\n') + '\n\n';
+
+  if (warningsCount > 0) {
+    output += '  ' + chalk.yellow(`${warningsCount} ${plur('warning', warningsCount)}`) + '\n';
+  }
+
+  if (errorCount > 0) {
+    output += '  ' + chalk.red(`${errorCount} ${plur('error', errorCount)}`) + '\n';
+  }
+
+  return (errorCount + warningsCount) > 0 ? output : '';
+};
