10e3236b2cb5a1c814231a1e184fd0927d98c634
Marc Görtz <mail@marcgoertz.de>
2017-06-04 11:55:55 +0200

    Add tests for empty or invalid Stylelint data, refs #2.

--- test/test.js
+++ test/test.js
@@ -7,14 +7,12 @@ import invalidOptionsFixture from './fixtures/invalidOptions';
 
 test('output', t => {
   const output = m(defaultFixture);
-  console.log(output);
   t.regex(stripAnsi(output), /first\.css:3:12\n/);
   t.regex(stripAnsi(output), /✖[ ]{3}3:12[ ]{2}Unexpected leading zero[ ]{26}number-leading-zero/);
 });
 
 test('deprecations', t => {
   const output = m(deprecationsFixture);
-  console.log(output);
   t.regex(stripAnsi(output), /Stylelint Configuration\n/);
   t.regex(stripAnsi(output), /ℹ time-no-imperceptible has been deprecated and in 8.0 will be removed. Instead use time-min-milliseconds with 100 as its primary option./);
   t.regex(stripAnsi(output), /ℹ block-no-single-line has been deprecated and in 8.0 will be removed. Instead use block-opening-brace-newline-after and block-closing-brace-newline-before with the always option./);
@@ -23,9 +21,18 @@ test('deprecations', t => {
 
 test('invalid options', t => {
   const output = m(invalidOptionsFixture);
-  console.log(output);
   t.regex(stripAnsi(output), /Stylelint Configuration\n/);
   t.regex(stripAnsi(output), /✖ Invalid option value snakeCase for rule value-keyword-case/);
   t.regex(stripAnsi(output), /✖ Unexpected option value always for rule no-unknown-animations/);
   t.regex(stripAnsi(output), /2 invalid options/);
+});
+
+test('empty results', t => {
+  const output = m([]);
+  t.is(stripAnsi(output), '');
+});
+
+test('invalid Stylelint output', t => {
+  const output = m('foobar');
+  t.is(stripAnsi(output), '');
 });
